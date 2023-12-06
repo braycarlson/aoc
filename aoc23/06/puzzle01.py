4e909c9879f4166a07630f91fdde36708012c136
Brayden Carlson <braycarlson@hotmail.com>
2023-12-05 22:41:44 -0700

    Sixth day

---
+++ aoc23/06/puzzle01.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+from math import prod
+from pathlib import Path
+
+
+def main() -> None:
+    path = Path.cwd().joinpath('input/input.txt')
+
+    with open(path, 'r') as handle:
+        text = handle.read().split()
+
+        information = [
+            int(x)
+            for x in text
+            if x.isdigit()
+        ]
+
+        middle = (len(information) // 2)
+
+        races = [
+            (information[i], information[middle + i])
+            for i in range(middle)
+        ]
+
+        error = []
+
+        for race in races:
+            time, distance = race
+
+            winnable = 0
+
+            for millisecond in range(0, time, 1):
+                potential = 0 if millisecond == 0 else (time - millisecond)
+                forward = 0 if millisecond == time - 1 else (potential * millisecond)
+
+                if forward > distance:
+                    winnable = winnable + 1
+
+            error.append(winnable)
+
+        total = prod(error)
+        print(total)
+
+
+if __name__ == '__main__':
+    main()
---
+++ aoc23/06/puzzle02.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+from math import prod
+from pathlib import Path
+
+
+def main() -> None:
+    path = Path.cwd().joinpath('input/input.txt')
+
+    with open(path, 'r') as handle:
+        text = handle.read().split()
+
+        information = [
+            x
+            for x in text
+            if x.isdigit()
+        ]
+
+        middle = (len(information) // 2)
+
+        time, distance = (
+            int(''.join(information[:middle])),
+            int(''.join(information[middle:]))
+        )
+
+        error = []
+
+        winnable = 0
+
+        for millisecond in range(0, time, 1):
+            potential = 0 if millisecond == 0 else (time - millisecond)
+            forward = 0 if millisecond == time - 1 else (potential * millisecond)
+
+            if forward > distance:
+                winnable = winnable + 1
+
+        error.append(winnable)
+
+        total = prod(error)
+        print(total)
+
+
+if __name__ == '__main__':
+    main()
